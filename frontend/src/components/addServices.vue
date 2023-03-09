1448e5aef9487d1551a55e275f7d8a2d35b63f51
clydesteapp <clydetristansteapp@gmail.com>
2023-03-09 11:05:13 -0600

    Frontend - Add Services and Find Services Pages

---
+++ frontend/src/components/addServices.vue
@@ -0,0 +1,187 @@
+<script>
+import useVuelidate from '@vuelidate/core'
+import { required, email, alpha, numeric } from '@vuelidate/validators'
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+
+// will have to go back and change this to route to the services collection in mongodb in sprint 3
+export default {
+  setup() {
+    return { v$: useVuelidate({ $autoDirty: true }) }
+  },
+  data() {
+    return {
+      org: '',
+      client: {
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: {
+          primary: '',
+          alternate: ''
+        },
+        address: {
+          line1: '',
+          line2: '',
+          city: '',
+          county: '',
+          zip: ''
+        }
+      }
+    }
+  },
+  created() {
+    axios.get(`${apiURL}/org`).then((res) => {
+      this.org = res.data._id
+    })
+  },
+  mounted() {
+    window.scrollTo(0, 0)
+  },
+  methods: {
+    // will have to change this in sprint 3 to services instead of clients, "addService"
+    registerClient() {
+      this.v$.$validate().then((valid) => {
+        if (valid) {
+          axios
+            .get(`${apiURL}/clients/lookup/${this.client.phoneNumber.primary}`)
+            .then((res) => {
+              if (res.data) {
+                if (res.data.orgs.includes(this.org)) {
+                  alert('Client phone number has already been registered.')
+                  this.$router.push({ name: 'findclient' })
+                } else {
+                  axios
+                    .put(`${apiURL}/clients/register/${res.data._id}`)
+                    .then(() => {
+                      alert('Client registered')
+                      this.$router.push({ name: 'findclient' })
+                    })
+                    .catch((error) => {
+                      console.log(error)
+                    })
+                }
+              } else {
+                axios
+                  .post(`${apiURL}/clients`, this.client)
+                  .then(() => {
+                    alert('Client added')
+                    this.$router.push({ name: 'findclient' })
+                  })
+                  .catch((error) => {
+                    console.log(error)
+                  })
+              }
+            })
+        }
+      })
+    }
+  },
+  // sets validations for the various data properties, will have to change in sprint 3 to services instead of clients
+  validations() {
+    return {
+      client: {
+        firstName: { required, alpha },
+        lastName: { required, alpha },
+        email: { email },
+        address: {
+          city: { required }
+        },
+        phoneNumber: {
+          primary: { required, numeric }
+        }
+      }
+    }
+  }
+}
+</script>
+<template>
+  <main>
+    <h1
+      class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+    >
+      Add Services Form
+    </h1>
+    <div class="px-10 py-20">
+      <!-- @submit.prevent stops the submit event from reloading the page-->
+      <!-- Will have to change in Sprint 3 to services instead of client, "addService"-->
+      <form @submit.prevent="registerClient">
+        <!-- grid container -->
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        >
+          <h2 class="text-2xl font-bold">Service Details</h2>
+          <!-- form field -->
+          <div class="flex flex-col">
+            <label class="block">
+              <span class="text-gray-700">Service Title</span>
+              <span style="color: #ff0000">*</span>
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                v-model="client.firstName"  
+              /> <!--will have to change in sprint 3 to services instead of client, "services.serviceName"-->
+              <span class="text-black" v-if="v$.client.firstName.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.client.firstName.$errors"
+                  :key="error.$uid"
+                >
+                  {{ error.$message }}!
+                </p>
+              </span>
+            </label>
+          </div>
+
+          <!-- form field -->
+          <div class="flex flex-col">
+            <label class="block">
+              <span class="text-gray-700">Description</span>
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder
+                v-model="client.middleName"
+              /> <!--will have to change in sprint 3 to services instead of client, "services.description"-->
+            </label>
+          </div>
+        </div>
+        <!-- grid container -->
+        <div
+          class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        >
+          <h2 class="text-2xl font-bold">Event Details</h2>
+          <!-- form field -->
+          <div class="flex flex-col">
+            <label class="block">
+              <span class="text-gray-700">Event</span>
+              <span style="color: #ff0000">*</span>
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                v-model="client.address.city"
+              /> <!--will have to change in sprint 3 to services instead of client, "services.eventName"-->
+              <span class="text-black" v-if="v$.client.address.city.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.client.address.$errors"
+                  :key="error.$uid"
+                >
+                  {{ error.$message }}!
+                </p>
+              </span>
+            </label>
+          </div>
+          <div></div>
+          <!-- submit button -->
+          <div class="flex justify-between mt-10 mr-20">
+            <button class="bg-red-700 text-white rounded" type="submit">
+              Add Service
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </main>
+</template>
---
+++ frontend/src/components/findServices.vue
@@ -0,0 +1,80 @@
+<script>
+import { DateTime } from 'luxon'
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Find Services
+      </h1>
+    </div>
+    <div class="px-10 pt-20">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <h2 class="text-2xl font-bold">Search Service</h2>
+        <!-- Displays Client Name search field -->
+        <div class="flex flex-col">
+          <label class="block">
+            <input
+              type="text"
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="name"
+              v-on:keyup.enter="handleSubmitForm"
+              placeholder="Enter Service Name"
+            />
+          </label>
+        </div>
+      </div>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <div></div>
+        <div></div>
+        <div class="mt-5 grid-cols-2">
+          <button
+            class="mr-10 border border-red-700 bg-white text-red-700 rounded"
+            @click="clearSearch"
+            type="submit"
+          >
+            Clear Search
+          </button>
+          <button
+            class="bg-red-700 text-white rounded"
+            @click="handleSubmitForm"
+            type="submit"
+          >
+            Search Service
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <hr class="mt-10 mb-10" />
+    <!-- Display Found Data -->
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+    >
+      <div class="ml-10">
+        <h2 class="text-2xl font-bold">Find Services</h2>
+        <h3 class="italic">Click table row to edit/display an entry</h3>
+      </div>
+      <div class="flex flex-col col-span-2">
+        <table class="min-w-full shadow-md rounded">
+          <thead class="bg-gray-50 text-xl">
+            <tr>
+              <th class="p-4 text-left">Service Name</th>
+              <th class="p-4 text-left">Service Description</th>
+              <th class="p-4 text-left">Events</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </main>
+</template>
